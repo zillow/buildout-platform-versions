4e5381b8815f639f9cf43e005d8d520c625870ba
Jonathan Ultis <jonathan@ultis.com>
2013-11-23 02:36:08 -0800

    replace pushd/popd since they aren't working

--- setup_clean_virtualenv.sh
+++ setup_clean_virtualenv.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 mkdir -p ~/env
-pushd ~/env
+CURDIR=`pwd`
+cd ~/env
 curl -O  https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.10.1.tar.gz
 tar -xvzf virtualenv-1.10.1.tar.gz
 python virtualenv-1.10.1/virtualenv.py ~/env/clean --no-setuptools --no-pip --no-site-packages
-popd
+cd $CURDIR
+
