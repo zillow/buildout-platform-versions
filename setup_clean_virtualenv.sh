fd5ad30c46c5df980573a2151e37986cd46d46d7
Jonathan Ultis <jonathan@ultis.com>
2013-11-23 02:32:30 -0800

    check for popd typo

--- setup_clean_virtualenv.sh
+++ setup_clean_virtualenv.sh
@@ -5,4 +5,3 @@ curl -O  https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.10.1.
 tar -xvzf virtualenv-1.10.1.tar.gz
 python virtualenv-1.10.1/virtualenv.py ~/env/clean --no-setuptools --no-pip --no-site-packages
 popd
-
